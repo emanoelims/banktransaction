3962223e08e22f0d6da0a0ce0b4db0953fc8c867
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-10 23:49:44 -0700

    feat: calculate total transactions in january

--- src/main/java/dev/emmanoel/banktransaction/BankTransactionSimpleAnalyzer.java
+++ src/main/java/dev/emmanoel/banktransaction/BankTransactionSimpleAnalyzer.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionSimpleAnalyzer {
@@ -20,5 +23,17 @@ public class BankTransactionSimpleAnalyzer {
             total += amount;
         }
         System.out.printf("The Total of all transactions is %.2f\n", total);
+
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        total = 0d;
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+        System.out.printf("The Total of transactions in month January is %.2f\n", total);
     }
 }
