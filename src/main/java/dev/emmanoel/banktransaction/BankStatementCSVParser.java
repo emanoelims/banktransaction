be482f253f6770db451277191f66cc51f99a9853
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-07-14 14:32:56 -0700

    refactor: update method name

--- src/main/java/dev/emmanoel/banktransaction/BankStatementCSVParser.java
+++ src/main/java/dev/emmanoel/banktransaction/BankStatementCSVParser.java
@@ -10,7 +10,7 @@ public class BankStatementCSVParser implements BankStatementParser {
         DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @Override
-    public BankTransaction parseFrom(final String line) {
+    public BankTransaction parseLineFrom(final String line) {
         final String[] columns = line.split(",");
         final LocalDate date = LocalDate.parse(columns[0], DATE_FORMATTER);
         final double amount = Double.parseDouble(columns[1]);
@@ -22,7 +22,7 @@ public class BankStatementCSVParser implements BankStatementParser {
     public List<BankTransaction> parseLinesFrom(final List<String> lines) {
         final List<BankTransaction> bankTransactions = new ArrayList<>();
         for (final String line : lines) {
-            bankTransactions.add(parseFrom(line));
+            bankTransactions.add(parseLineFrom(line));
         }
         return bankTransactions;
     }
--- src/main/java/dev/emmanoel/banktransaction/BankStatementParser.java
+++ src/main/java/dev/emmanoel/banktransaction/BankStatementParser.java
@@ -3,7 +3,7 @@ package dev.emmanoel.banktransaction;
 import java.util.List;
 
 public interface BankStatementParser {
-    BankTransaction parseFrom(String line);
+    BankTransaction parseLineFrom(String line);
 
     List<BankTransaction> parseLinesFrom(List<String> lines);
 }
